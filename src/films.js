2659f61a08ac1864ef738793becf505b90fe1d76
abril <abicelaya@gmail.com>
2024-05-16 12:06:57 +0200

    completar funcion `getMoviesFromDirector`

--- src/films.js
+++ src/films.js
@@ -8,7 +8,11 @@ function getAllDirectors(array) {
 }
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {}
+function getMoviesFromDirector(array, director) {
+  let result = array.filter((movie) => movie.director === director);
+  console.log(result);
+  return result;
+}
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {}
